d2911cfcfd77266411071a4d395a49333fae57df
andcebrod <acrmontellano@gmail.com>
2019-04-01 12:53:26 +0200

    Usando constantes

--- Refactorizacion/src/es/studium/Refactorizacion/calcularPrecio.java
+++ Refactorizacion/src/es/studium/Refactorizacion/calcularPrecio.java
@@ -2,8 +2,10 @@ package es.studium.Refactorizacion;
 
 public class calcularPrecio {
 
+	
 	public double calcularPrecio(int unidades, double precioUnitario) 
 	{
-		return (unidades*precioUnitario)*(1.21);
+		final double iva = 1.21;
+		return (unidades*precioUnitario)*(iva);
 	}
 }
