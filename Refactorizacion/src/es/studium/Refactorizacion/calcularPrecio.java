cecc9334f9e4ec1a9642c11943a7603f0123b496
andcebrod <acrmontellano@gmail.com>
2019-04-01 12:08:13 +0200

    Ejercicio precio

---
+++ Refactorizacion/src/es/studium/Refactorizacion/calcularPrecio.java
@@ -0,0 +1,9 @@
+package es.studium.Refactorizacion;
+
+public class calcularPrecio {
+
+	public double calcularPrecio(int unidades, double precioUnitario) 
+	{
+		return unidades*precioUnitario;
+	}
+}
